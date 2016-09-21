dc297299d6a77a5255851f3db6928fd0d89ab009
kidpixo <kidpixo@gmail.com>
2016-09-21 13:48:46 +0200

    modified the docker executable location in scripts

--- _scripts/dockerps2csv.sh
+++ _scripts/dockerps2csv.sh
@@ -1,2 +1,2 @@
 echo "ID,Image,Command,CreatedAt,RunningFor,Ports,Status,Size,Names,Labels,Mounts"
-/usr/local/bin/docker ps --format '"{{.ID}}","{{.Image}}",{{.Command}},"{{.CreatedAt}}","{{.RunningFor}}","{{.Ports}}","{{.Status}}","{{.Size}}","{{.Names}}","{{.Labels}}","{{.Mounts}}"'
+/usr/bin/docker ps --format '"{{.ID}}","{{.Image}}",{{.Command}},"{{.CreatedAt}}","{{.RunningFor}}","{{.Ports}}","{{.Status}}","{{.Size}}","{{.Names}}","{{.Labels}}","{{.Mounts}}"'
